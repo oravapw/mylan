139e29699dcdddaee376129c46069a6e14e8b61e
Petri Wessman <orava@iki.fi>
2022-06-29 22:17:32 +0300

    added on-the-fly player creation, app is now done in theory

--- app/controllers/tournament_players_controller.rb
+++ app/controllers/tournament_players_controller.rb
@@ -1,25 +1,46 @@
 class TournamentPlayersController < ApplicationController
 
+  before_action :load_player, only: [:destroy, :toggle_decklist]
+  before_action :load_tournament, only: [:new, :create]
+
   def new
+    @player = TournamentPlayer.new
   end
 
+  # this is a bit weird, since we handle multiple scenarios here and the result processing is with turbo streams
   def create
-    @player = TournamentPlayer.new(tournament_player_params)
-    @tournament = Tournament.find(params[:tournament_id])
-    @player.tournament = @tournament
-    unless @player.save
-      render :new, status: :unprocessable_entity
+    @cancel = params[:cancel]
+    unless @cancel
+      # base functionality has us just creating  tournament-specific player and linking it in
+      @player = TournamentPlayer.new(tournament_player_params)
+      @player.tournament = @tournament
+
+      # optionally create an "actual" player object (this is when user selects "Add new" option on search)
+      if params[:create_player] == "true"
+        @baseplayer = Player.new
+        @baseplayer.name = @player.name
+        @baseplayer.vekn = @player.vekn
+        @baseplayer.country = @player.country
+        if @baseplayer.save
+          @player.player_id = @baseplayer.id
+        else
+          # do not show validation errors about player_id in this scenario, since form validation uses
+          # TournamentPlayer object and not Player
+          @player.skip_playerid_check = true
+        end
+        @player.prereg = false
+      end
+
+      @player.save
     end
   end
 
   def destroy
-    @player = TournamentPlayer.find(params[:id])
     @tournament = @player.tournament
     @player.destroy
   end
 
   def toggle_decklist
-    @player = TournamentPlayer.find(params[:id])
     @player.decklist = !@player.decklist
     @player.save!
     render partial: "tournaments/playerlist", locals: { tournament: @player.tournament }
@@ -27,8 +48,16 @@ class TournamentPlayersController < ApplicationController
 
   private
 
+  def load_player
+    @player = TournamentPlayer.find(params[:id])
+  end
+
+  def load_tournament
+    @tournament = Tournament.find(params[:tournament_id])
+  end
+
   def tournament_player_params
-    params.require(:tournament_player).permit(:name, :vekn, :prereg, :player_id, :decklist)
+    params.require(:tournament_player).permit(:name, :vekn, :prereg, :player_id, :decklist, :country)
   end
 
 end
--- app/models/tournament_player.rb
+++ app/models/tournament_player.rb
@@ -1,9 +1,12 @@
 class TournamentPlayer < ApplicationRecord
   belongs_to :tournament
 
+  attr_accessor :country
+  attr_accessor :skip_playerid_check
+
   validates :name, presence: true, length: { maximum: 40 }
   validates :vekn, allow_blank: true, length: { is: 7 }, numericality: { only_integer: true }
-  validates :player_id, presence: true, numericality: { only_integer: true }
+  validates :player_id, presence: true, numericality: { only_integer: true }, unless: -> { skip_playerid_check }
 
   def self.update_player_data(player_id, prereg, name, vekn)
     self.where(player_id: player_id, prereg: prereg).find_each do |p|
