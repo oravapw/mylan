a24e24bdeb29a04a68c63b2fe36a00dfaf2a5fb6
Petri Wessman <orava@iki.fi>
2023-06-28 15:20:17 +0300

    added per-country display to public registration page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,12 +8,18 @@ class RegistrationsController < ApplicationController
       return
     end
 
-    # load in country info for players
+    @per_country = []
+
     if @tournament.tournament_players.present?
+      # load in country info for players
       ids = @tournament.tournament_players.map { |tp| tp.player_id }
       id_to_c = {}
       Player.where(id: ids).pluck(:id, :country).each { |result| id_to_c[result[0]] = result[1] }
       @tournament.tournament_players.each { |tp| tp.country = id_to_c[tp.player_id] }
+
+      # calculate per-country totals into array of arrays, in descending count order
+      @per_country = @tournament.tournament_players.map { |tp| tp.country_name }
+                                .tally.sort_by { |_key, value| value }.reverse
     end
   end
 
